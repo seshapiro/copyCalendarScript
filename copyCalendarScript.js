b2772e9d602545c73bee4e8965ca8e5b9d8b9424
roastedbagel <roastedbagel@gmail.com>
2023-09-04 04:23:43 -0400

    Prod

--- copyCalendarScript.js
+++ copyCalendarScript.js
@@ -1,83 +1,87 @@
-const SECONDARY_CALENDAR_ID = "yourcalendar@gmail.com";
-// primary calendar is your "work" calendar where secondary events will be copied over as "Busy" slots
-const PRIMARY_CALENDAR_ID = "yourcalendar@work.com";
+// ****************************************************************************
+// Copy Calendar Script
+// SS 09/03/2023 - V3 Production
+// ****************************************************************************
 
-// *******************
+// work calendar is your "work" calendar or one you'll copy events from
+const WORK_CALENDAR_ID = "s.shapiro@hellobluewave.com";
+// personal calendar is your "personal" calendar where work (work) events will be copied over as "Busy" slots
+const PERSONAL_CALENDAR_ID = "1d17eb7e389388b9db58b2683307d5d0e57f08f4d86bfbc32f645a1cde6d875c@group.calendar.google.com";
+
+// ****************************************************************************
 // These you can customize if you want
-// *******************
+// ****************************************************************************
 
-// How many days ahead do you want to block off time in your primary calendar
-const DAYS_LOOKAHEAD = 14;
-// What title do your secondary events have in your primary calendar
+// How many days ahead do you want to block off time in your personal calendar
+const DAYS_LOOKAHEAD = 3;
+// What title do your work events have in your personal calendar
 const BUSY_EVENT_TITLE = "Busy";
-// Override your usual primary calendar event color for copied Busy events. 
-// From https://developers.google.com/apps-script/reference/calendar/event-color.html
-// If you don't want to override, comment out the place this constant is used.
-const BUSY_EVENT_COLOR_ID = CalendarApp.EventColor.GRAY;
-// ignore secondary events that end before this time (in 24 hr time)
-const IGNORE_SECONDARY_EVENTS_BEFORE = 9
-// ignore secondary events that start after this time (in 24 hr time)
-const IGNORE_SECONDARY_EVENTS_AFTER = 17
-// ignore secondary events over weekends
-const IGNORE_SECONDARY_EVENTS_ON_WEEKENDS = true
+// Override your usual personal calendar event color for copied Busy events. 
+const BUSY_EVENT_COLOR_ID = CalendarApp.EventColor.ORANGE;
+// ignore work events that end before this time (in 24 hr time)
+const IGNORE_WORK_EVENTS_BEFORE = 8
+// ignore work events that start after this time (in 24 hr time)
+const IGNORE_WORK_EVENTS_AFTER = 23
+// ignore work events over weekends
+const IGNORE_WORK_EVENTS_ON_WEEKENDS = true
 
-// *******************
-// Below here is code you can look through and tweak if you want, but most of the customization
-// should be above.
-// *******************
 
-// source: https://gist.github.com/ttrahan/a88febc0538315b05346f4e3b35997f2
-// blog: https://chromatichq.com/blog/syncing-your-personal-work-calendars
-// original:
-// https://medium.com/@willroman/auto-block-time-on-your-work-google-calendar-for-your-personal-events-2a752ae91dab
 function exec() {
   const today = new Date();
   const enddate = new Date();
   enddate.setDate(today.getDate() + DAYS_LOOKAHEAD); // how many days in advance to monitor and block off time
 
-  const secondaryCal = CalendarApp.getCalendarById(SECONDARY_CALENDAR_ID);
-  const secondaryEvents = secondaryCal.getEvents(today, enddate);
+  const workCal = CalendarApp.getCalendarById(WORK_CALENDAR_ID);
+  const workEvents = workCal.getEvents(today, enddate);
 
-  const primaryCal = CalendarApp.getCalendarById(PRIMARY_CALENDAR_ID);
-  const primaryEvents = primaryCal.getEvents(today, enddate); // all primary calendar events
+  const personalCal = CalendarApp.getCalendarById(PERSONAL_CALENDAR_ID);
+  const personalEvents = personalCal.getEvents(today, enddate); // all personal calendar events
 
-  const primaryEventTitle = BUSY_EVENT_TITLE; 
+  const personalEventTitle = BUSY_EVENT_TITLE; 
 
   let evi, existingEvent;
-  const primaryEventsFiltered = []; // primary events that were previously created from secondary
-  const primaryEventsUpdated = []; // primary events that were updated from secondary calendar
-  const primaryEventsCreated = []; // primary events that were created from secondary calendar
-  const primaryEventsDeleted = []; // primary events previously created that have been deleted from secondary
+  const personalEventsFiltered = []; // personal events that were previously created from work
+  const personalEventsUpdated = []; // personal events that were updated from work calendar
+  const personalEventsCreated = []; // personal events that were created from work calendar
+  const personalEventsDeleted = []; // personal events previously created that have been deleted from work
+  const personalEventsString = personalEventsCreated.toString();
 
-  Logger.log("Number of primaryEvents: " + primaryEvents.length);
-  Logger.log("Number of secondaryEvents: " + secondaryEvents.length);
 
-  // create filtered list of existing primary calendar events that were previously created from the secondary calendar
-  for (let pev in primaryEvents) {
-    const pEvent = primaryEvents[pev];
-    if (pEvent.getTitle() === primaryEventTitle) { primaryEventsFiltered.push(pEvent); }
+  Logger.log("Number of personalEvents: " + personalEvents.length);
+  Logger.log("Number of workEvents: " + workEvents.length);
+
+  // process all events in work calendar
+  for (let sev in workEvents) {
+    let canSkip = false;
+    evi = workEvents[sev];
+
+  // create filtered list of existing personal calendar events that were previously created from the work calendar
+  for (let pev in personalEvents) {
+    const pEvent = personalEvents[pev];
+    if (pEvent.getTitle() === evi.getTitle()) { personalEventsFiltered.push(pEvent); }
   }
 
-  // process all events in secondary calendar
-  for (let sev in secondaryEvents) {
-    let canSkip = false;
-    evi = secondaryEvents[sev];
-
-    // if the secondary event has already been blocked in the primary calendar, update it
-    for (existingEvent in primaryEventsFiltered) {
-      const pEvent = primaryEventsFiltered[existingEvent];
+    // if the work event has already been blocked in the personal calendar, update it
+    for (existingEvent in personalEventsFiltered) {
+      const pEvent = personalEventsFiltered[existingEvent];
       const isSameStart = pEvent.getStartTime().getTime() === evi.getStartTime().getTime();
       const isSameEnd = pEvent.getEndTime().getTime() === evi.getEndTime().getTime();
-      if (isSameStart && isSameEnd) {
+      const isSameTitle = pEvent.getTitle() === evi.getTitle();
+      const eventTitle = evi.getTitle();
+      const eventGuests = evi.getGuestList(true);
+      const eventDesc = evi.getDescription();
+
+      if (isSameStart && isSameEnd && isSameTitle) {
         canSkip = true;
 
         // There's probably no carry updates as long as the only thing you're syncing is "Busy". If you wanted to
         // copy over more info, this would be the place to re-copy updates
 
-        // pEvent.setDescription(secondaryTitle + '\n\n' + secondaryDesc);
+        pEvent.setDescription(eventDesc);
+        pEvent.setTitle(eventTitle);
         // etc...
 
-        primaryEventsUpdated.push(pEvent.getId());
+        personalEventsUpdated.push(pEvent.getId());
       }
     }
 
@@ -87,46 +91,51 @@ function exec() {
       continue;
     }
 
-    // if the secondary event does not exist in the primary calendar, create it
+    // if the work event does not exist in the personal calendar, create it
     // we use the Calendar API (instead of the CalendarApp given to us) because it allows us to specify not using
     // default reminders, so we aren't notified about meaningless "Busy" events.
+
+    const eventTitle = evi.getTitle();
+    const eventGuests = evi.getGuestList(true);
+    const eventDesc = evi.getDescription();
     const event = {
-      summary: primaryEventTitle,
+      summary: eventTitle,
       start: {
         dateTime: evi.getStartTime().toISOString(),
       },
       end: {
         dateTime: evi.getEndTime().toISOString(),
       },
+      description: eventDesc, //+ '\n\n' + eventGuests.getName,
       colorId: BUSY_EVENT_COLOR_ID,
       reminders: {
-        useDefault: false,
+        useDefault: true,
       },
     };
-    const newEvent = Calendar.Events.insert(event, PRIMARY_CALENDAR_ID);
-    primaryEventsCreated.push(newEvent.id);
-    Logger.log("PRIMARY EVENT CREATED", newEvent);
+    const newEvent = Calendar.Events.insert(event, PERSONAL_CALENDAR_ID);
+    personalEventsCreated.push(newEvent.id);
+    Logger.log("PERSONAL EVENT CREATED: " + newEvent.summary + " ||| " + newEvent.start + "-" + newEvent.end);
   }
 
-  // if a primary event previously created no longer exists in the secondary calendar, delete it
-  for (pev in primaryEventsFiltered) {
-    const pevIsUpdatedIndex = primaryEventsUpdated.indexOf(primaryEventsFiltered[pev].getId());
+  // if a personal event previously created no longer exists in the work calendar, delete it
+  for (pev in personalEventsFiltered) {
+    const pevIsUpdatedIndex = personalEventsUpdated.indexOf(personalEventsFiltered[pev].getId());
     if (pevIsUpdatedIndex === -1) {
-      const pevIdToDelete = primaryEventsFiltered[pev].getId();
+      const pevIdToDelete = personalEventsFiltered[pev].getId();
       Logger.log(pevIdToDelete + " deleted");
-      primaryEventsDeleted.push(pevIdToDelete);
-      primaryEventsFiltered[pev].deleteEvent();
+      personalEventsDeleted.push(pevIdToDelete);
+      personalEventsFiltered[pev].deleteEvent();
     }
   }
 
-  Logger.log("Primary events previously created: " + primaryEventsFiltered.length);
-  Logger.log("Primary events no change: " + primaryEventsUpdated.length);
-  Logger.log("Primary events deleted: " + primaryEventsDeleted.length);
-  Logger.log("Primary events created: " + primaryEventsCreated.length);
+  Logger.log("Personal events previously created: " + personalEventsFiltered.length);
+  Logger.log("Personal events no change: " + personalEventsUpdated.length);
+  Logger.log("Personal events deleted: " + personalEventsDeleted.length);
+  Logger.log("Personal events created: " + personalEventsCreated.length);
 
 }
 
-// You can update the conditions where you do not copy secondary events over as "Busy"
+// You can update the conditions where you do not copy work events over as "Busy"
 function shouldIgnore(event) {
   // Do nothing if the event is an all-day or multi-day event. This script only syncs hour-based events
   if (event.isAllDayEvent()) {
@@ -134,16 +143,16 @@ function shouldIgnore(event) {
   }
 
   // skip events that end by 9 AM
-  if (event.getEndTime().getHours() <= IGNORE_SECONDARY_EVENTS_BEFORE) {
+  if (event.getEndTime().getHours() <= IGNORE_WORK_EVENTS_BEFORE) {
     return true;
   }
 
   // skip events that start after 5pm
-  if (event.getStartTime().getHours() >= IGNORE_SECONDARY_EVENTS_AFTER) {
+  if (event.getStartTime().getHours() >= IGNORE_WORK_EVENTS_AFTER) {
     return true;
   }
 
-  if (IGNORE_SECONDARY_EVENTS_ON_WEEKENDS) {
+  if (IGNORE_WORK_EVENTS_ON_WEEKENDS) {
     const date = event.getStartTime();
     const dayNum = date.getDay();
     if (dayNum === 0 || dayNum === 6) {
